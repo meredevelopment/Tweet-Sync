6b92bc32edd6e422694f8b44539bd500ddd4565f
Dan Gates <dan.gates@me.com>
2013-11-07 16:23:26 +0000

    More tweaks for tagging a release.

--- twitter.php
+++ twitter.php
@@ -9,10 +9,10 @@
  *
  * @category  TweetSync
  * @package   Wordpress
- * @author    Dan Gates <dan@by-robots.com>
- * @copyright 2013 NaN Bug/Dan Gates
+ * @author    By Robots
+ * @copyright 2013 By Robots
  * @license   Usage strictly forbidden.
- * @link      https://github.com/dangates/Tweet-Sync
+ * @link      https://github.com/by-robots/Tweet-Sync
  */
 
 /**
@@ -20,9 +20,9 @@
  *
  * @category TweetSync
  * @package  Wordpress
- * @author   Dan Gates <dan@by-robots.com>
+ * @author   By Robots
  * @license  Usage strictly forbidden.
- * @link     https://github.com/dangates/Tweet-Sync
+ * @link     https://github.com/by-robots/Tweet-Sync
  */
 class Twitter
 {
